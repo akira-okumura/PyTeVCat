0aea8e7774f5e78d229a81ccf8d7752de463e202
oxon <oxon@2c8fa7c8-c7d1-4b06-b319-fff884cebd0f>
2011-12-16 13:31:35 +0000

    Added SVN keyword

--- PyTeVCat/src/setup.py
+++ PyTeVCat/src/setup.py
@@ -3,6 +3,8 @@ Setup script for PyTeVCat
 $Id$
 """
 
+import tevcat
+
 from numpy.distutils.core import setup
 
 setup(name="PyTeVCat",
@@ -10,6 +12,16 @@ setup(name="PyTeVCat",
       description="Python wrapper for TeVCat",
       author="Akira Okumura",
       author_email="oxon@mac.com",
+      url='https://sourceforge.net/p/pytevcat/',
+      license='BSD License',
+      platforms=['MacOS :: MacOS X', 'POSIX', 'Windows'],
       packages=["tevcat"],
+      install_requires=['coords'],
       package_data={"tevcat": ["img/*.png",]},
+      classifiers=['Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Development Status :: 4 - Beta',
+                   'Programming Language :: Python',
+                   ],
+      long_description=tevcat.__doc__
       )
--- PyTeVCat/src/tevcat/__init__.py
+++ PyTeVCat/src/tevcat/__init__.py
@@ -1,5 +1,11 @@
 """
+PyTeVCat is a python wrapper of TeVCat
+http://tevcat.uchicago.edu/
+
+It retrieves the TeV source database from the TeVCat HTML. Basic getter methods
+and an all-sky viewer are offered.
+
+$Id$
 """
-#$Id$
 
 from tevcat_all import *
