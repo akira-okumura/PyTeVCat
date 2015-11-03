4bb21d092a121964cd446ea86dc64f8af67b1b3d
oxon <oxon@mac.com>
2015-11-03 15:41:25 +0100

    - Change the version number to 1.1.7 - Change the development status to "5 - Production/Stable"

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 """
 Setup script for PyTeVCat
-$Id$
 """
 
 from numpy.distutils.core import setup
 
 setup(name="PyTeVCat",
-      version="1.1.6",
+      version="1.1.7",
       description="Python wrapper for TeVCat",
       author="Akira Okumura",
       author_email="oxon@mac.com",
-      url='https://sourceforge.net/p/pytevcat/',
+      url='https://github.com/akira-okumura/PyTeVCat/',
       license='BSD License',
       platforms=['MacOS :: MacOS X', 'POSIX'],
       packages=["tevcat"],
@@ -18,7 +17,7 @@ setup(name="PyTeVCat",
       package_data={"tevcat": ["img/*.png",]},
       classifiers=['Topic :: Scientific/Engineering :: Astronomy',
                    'Topic :: Scientific/Engineering :: Physics',
-                   'Development Status :: 4 - Beta',
+                   'Development Status :: 5 - Production/Stable',
                    'Programming Language :: Python',
                    ],
       long_description='tevcat.Python interface for TeVCat (http://tevcat.uchicago.edu/)'
